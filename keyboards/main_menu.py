b70aac24a827aae38bec0d917aa68cff2326d41b
Александр Фокин <wegnagun@bk.ru>
2023-11-29 16:08:32 +0300

    add some keyboards

--- keyboards/main_menu.py
+++ keyboards/main_menu.py
@@ -1,2 +1,13 @@
-def set_main_menu():
-    pass
+from aiogram import Bot
+from aiogram.types import BotCommand
+
+from lexicon.lexicon import LEXICON_COMMANDS
+
+
+async def set_main_menu(bot: Bot):
+    """ Функция настройки кнопки Menu бота. """
+    main_menu_commands = [
+        BotCommand(command=command, description=description)
+        for command, description in LEXICON_COMMANDS.items()
+    ]
+    await bot.set_my_commands(main_menu_commands)
